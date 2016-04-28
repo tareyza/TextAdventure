73efbf02f1de9d63bd9f2db52b06e3b264fd410b
jagen31 <jargen3d@yahoo.com>
2016-04-28 15:06:25 -0400

    update Constants

--- src/textbasedadventurejj/Constants.java
+++ src/textbasedadventurejj/Constants.java
@@ -6,4 +6,5 @@ public final class Constants {
 	public static final String LOCATION_ROOT = "";
 	
 	public static final String EVENT_FILE = "events";
+	public static final String OBJECT_FILE = "objects";
 }
--- src/textbasedadventurejj/GameObjectManager.java
+++ src/textbasedadventurejj/GameObjectManager.java
@@ -17,7 +17,7 @@ public class GameObjectManager {
     public void loadObjects(){
     	objects = new HashMap<>();
     	try{
-    		for(String s : Utils.readFile(Constants.OBJECT_ROOT + "Objects").split("\n")){
+    		for(String s : Utils.readFile(Constants.OBJECT_ROOT + Constants.OBJECT_FILE).split("\n")){
     			String[] fullObj = s.split(":");
     			String name = fullObj[0];
     			GameObject object = Loader.loadObject(name);
