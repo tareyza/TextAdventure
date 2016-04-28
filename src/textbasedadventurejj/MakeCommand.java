3e64d101bdc8ed393b0e826dc2c7be9794b74a8a
tareyza <tareyza@yahoo.com>
2016-04-28 18:03:03 -0400

    misc stuff

--- src/textbasedadventurejj/MakeCommand.java
+++ src/textbasedadventurejj/MakeCommand.java
@@ -13,16 +13,20 @@ public class MakeCommand implements Command {
 
     @Override
     public boolean execute(String[] words) {
-        if (words.length < 1) {
+        if (words.length < 5) {
             return false;
-        } 
-        //"make object in location as name"
-        String object = words[0];
-        Location location = Interpreter.getInstance().getRoot().getSubLocation(words[2]);
-        String objectName = words[4];
-        
-        location.getChildren().put(objectName, new GameObject(object));
-        return true;
+        }
+        if (words[1].equals("in") && words[3].equals("as")) {
+            //"make object in location as name"
+            String object = words[0];
+            Location location = Interpreter.getInstance().getRoot().getSubLocation(words[2]);
+            String objectName = words[4];
+
+            location.getChildren().put(objectName, new GameObject(object));
+            return true;
+        } else {
+            return false;
+        }
     }
-    
+
 }
