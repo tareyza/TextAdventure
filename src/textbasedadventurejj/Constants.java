c99a79ccc987075f8f3f2148e7635b210b5436f1
jagen31 <jargen3d@yahoo.com>
2016-05-11 14:06:39 -0400

    made constants less annoying

--- src/textbasedadventurejj/Constants.java
+++ src/textbasedadventurejj/Constants.java
@@ -2,15 +2,16 @@ package textbasedadventurejj;
 
 public final class Constants {
 
-	public static final String ROOT = "C:\\Users\\quchen\\Documents\\GitHub\\TextAdventure\\beta\\";
+ 	public static final String ROOT ="C:/cygwin64/home/jarge_000/Coding/TBA/beta";
+	//public static final String ROOT = "C:/Users/quchen/Documents/GitHub/TextAdventure/beta/";
 
-	public static final String OBJECT_DIR = "Objects\\";
-	public static final String LOCATION_DIR = "Locations\\";
+	public static final String OBJECT_DIR = "Objects/";
+	public static final String LOCATION_DIR = "Locations/";
 	public static final String OBJECT_FILE = "objects";
 	public static final String EVENT_FILE = "events";
 	public static final String STRUCTURE_FILE = "structures";
 	public static final String SAVE_FILE = "save";
 	public static final String ERROR_FILE = "error";
-        public static final String GAME_OBJECT_FILE = "gameobjects";
-        public static final String NEW_GAME_FILE = "new";
+    public static final String GAME_OBJECT_FILE = "gameobjects";
+    public static final String NEW_GAME_FILE = "new";
 }
