e82c02707a372cc78cc71ee3b8b6bd5ef056c0e7
jagen31 <jargen3d@yahoo.com>
2016-05-13 14:39:20 -0400

    added debug command

--- src/textbasedadventurejj/DoCommand.java
+++ src/textbasedadventurejj/DoCommand.java
@@ -5,6 +5,10 @@ public class DoCommand implements Command {
 
 	@Override
 	public void execute(String[] words) {
+		System.out.println(java.util.Arrays.toString(words));
+		if(words.length == 2 && words[0].equals("debug")){
+			System.out.println(LocationManager.getInstance().getContext());
+		}
 		Interpreter.getInstance().interpretSentence(words);
 	}
 }
