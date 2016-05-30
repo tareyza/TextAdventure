a1b1e56e7a7ffba433e43b57d9b0099481a5ee8a
tareyza <tareyza@yahoo.com>
2016-05-30 15:14:05 -0400

    updated help text

--- src/textbasedadventurejj/TestLoader.java
+++ src/textbasedadventurejj/TestLoader.java
@@ -11,7 +11,7 @@ public class TestLoader {
         interp.addCommand("say", new SayCommand());
         interp.addCommand("do", new DoCommand());
 
-        System.out.println("Welcome to Jerry and Jared's text based adventure. \nHere's some basic tips: In general to travel between rooms you have to type \"enter door\" If there is more than door in a room, type something like \"enter hallway door\".\nType \"look around\" or \"examine\" to examine your surroundings and objects. \nMake sure you start each command you type in with a verb, like \"say yes\" or \"open door\"");
+        System.out.println("Welcome to Jerry and Jared's text based adventure. \nHere's some basic tips: In general to travel between rooms you have to type \"enter door\" If there is more than door in a room, type something like \"enter hallway door\".\nWhen in doubt, type \"look around\" or \"examine\" to examine your surroundings and objects and \"take\" to take items. \nMake sure you start each command you type in with a verb, like \"say yes\" or \"open door\"");
         System.out.println("");
         GameObjectManager gmanager = GameObjectManager.getInstance();
         LocationManager lmanager = LocationManager.getInstance();
