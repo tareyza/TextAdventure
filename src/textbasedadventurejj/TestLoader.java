d12e6c3484b7c2bfe413d8a3225263c5d8d1ed60
tareyza <tareyza@yahoo.com>
2016-05-31 22:38:54 -0400

    yay!!!!! now the gui part really works

--- src/textbasedadventurejj/TestLoader.java
+++ src/textbasedadventurejj/TestLoader.java
@@ -1,5 +1,6 @@
 package textbasedadventurejj;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
@@ -24,14 +25,21 @@ public class TestLoader {
 
         //gui stuff is below
         PipedInputStream inPipe = new PipedInputStream();
+        JFrame frame = new JFrame("Text Adventure");
+        frame.setLayout(new BorderLayout());
+        
         PipedInputStream outPipe = new PipedInputStream();
         System.setIn(inPipe);
         System.setOut(new PrintStream(new PipedOutputStream(outPipe), true));
-        PrintWriter inWriter = new PrintWriter(new PipedOutputStream(inPipe), true);
         
-        JFrame frame = new JFrame("Text Adventure");
-         frame.add(console(outPipe, inWriter));
-
+        PrintWriter inWriter = new PrintWriter(new PipedOutputStream(inPipe), true);
+        JTextArea jta = console(outPipe, inWriter);
+        frame.add(jta);
+        System.setIn(inPipe);
+        PrintStream con = new PrintStream(new TextAreaOutputStream(jta));
+        System.setOut(con);
+        System.setErr(con);
+        frame.add(new JScrollPane(jta));
         frame.pack();
         frame.setVisible(true);
         frame.setSize(800, 800);
@@ -48,38 +56,68 @@ public class TestLoader {
         RunGame.printNewGameText();
         RunGame.runGame();
     }
-    
+
     public static JTextArea console(final InputStream out, final PrintWriter in) {
-    final JTextArea area = new JTextArea();
+        final JTextArea area = new JTextArea();
 
-    // handle "System.out"
-    new SwingWorker<Void, String>() {
-        @Override protected Void doInBackground() throws Exception {
-            Scanner s = new Scanner(out);
-            while (s.hasNextLine()) publish(s.nextLine() + "\n");
-            return null;
-        }
-        @Override protected void process(List<String> chunks) {
-            for (String line : chunks) area.append(line);
-        }
-    }.execute();
-
-    // handle "System.in"
-    area.addKeyListener(new KeyAdapter() {
-        private StringBuffer line = new StringBuffer();
-        @Override public void keyTyped(KeyEvent e) {
-            char c = e.getKeyChar();
-            if (c == KeyEvent.VK_ENTER) {
-                in.println(line);
-                line.setLength(0); 
-            } else if (c == KeyEvent.VK_BACK_SPACE) { 
-                line.setLength(line.length() - 1); 
-            } else if (!Character.isISOControl(c)) {
-                line.append(e.getKeyChar());
+        // handle "System.out"
+        new SwingWorker<Void, String>() {
+            @Override
+            protected Void doInBackground() throws Exception {
+                Scanner s = new Scanner(out);
+                while (s.hasNextLine()) {
+                    publish(s.nextLine() + "\n");
+                }
+                return null;
             }
-        }
-    });
 
-    return area;
+            @Override
+            protected void process(List<String> chunks) {
+                for (String line : chunks) {
+                    area.append(line);
+                }
+            }
+        }.execute();
+
+        // handle "System.in"
+        area.addKeyListener(new KeyAdapter() {
+            private StringBuffer line = new StringBuffer();
+
+            @Override
+            public void keyTyped(KeyEvent e) {
+                char c = e.getKeyChar();
+                if (c == KeyEvent.VK_ENTER) {
+                    in.println(line);
+                    line.setLength(0);
+                } else if (c == KeyEvent.VK_BACK_SPACE) {
+                    line.setLength(line.length() - 1);
+                } else if (!Character.isISOControl(c)) {
+                    line.append(e.getKeyChar());
+                }
+            }
+        });
+
+        return area;
+    }
 }
-}
+
+/*
+        PipedInputStream inPipe = new PipedInputStream();
+        PipedInputStream outPipe = new PipedInputStream();
+        System.setIn(inPipe);
+        System.setOut(new PrintStream(new PipedOutputStream(outPipe), true));
+        PrintWriter inWriter = new PrintWriter(new PipedOutputStream(inPipe), true);
+        
+        JFrame frame = new JFrame("Text Adventure");
+        frame.setLayout(new BorderLayout());
+        JTextArea jta = console(outPipe, inWriter);
+        frame.add(jta);
+        //JScrollPane scrollPane = new JScrollPane(jta);
+        //frame.add(scrollPane,BorderLayout.EAST);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setSize(800, 800);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //end gui bullshit
+*/
